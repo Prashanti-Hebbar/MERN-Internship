022e9ad519f037f55c57ae6c4e2164f01076c3b4
Prashanti Hebbar <158825898+Prashanti-Hebbar@users.noreply.github.com>
2026-02-22 09:10:28 -0500

    Update TernaryOperator.jsx

--- MERN_Internship/myapp/src/components/ES7/TernaryOperator.jsx
+++ MERN_Internship/myapp/src/components/ES7/TernaryOperator.jsx
@@ -5,10 +5,11 @@ export default function TernaryOperator() {
     const score = 88
   return (
     <div style={score>=80 ? {background:'green',color:'white'} : score>=70 ? {background:'blue',color:'white'} : score>=60 ? {background:'orange',color:'white'} : {background:'red',color:'white'}}>
-        {score >= 80 ? <h2>Distinction</h2> :
-        score >= 70 ? <h2>First Class</h2> :
-        score >= 60 ? <h2>Second Class</h2> :
+        {score >= 80 ? <h2>Grade A</h2> :
+        score >= 70 ? <h2>Grade B</h2> :
+        score >= 60 ? <h2>Grade C</h2> :
         <h2>Fail</h2>}
     </div>
   )
+
 }
